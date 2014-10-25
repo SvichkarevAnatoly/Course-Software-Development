aedf7624eeb2f8cb1a4b9cd1e63cc3e5dc087602
svichkarev <tolik0393@mail.ru>
2014-10-25 17:36:12 +0700

    added template for Android library

---
+++ ShareAppsAndroidLibrary/src/ru/share_apps/ShareAppsAndroidLibrary/ShareApps.java
@@ -0,0 +1,13 @@
+package ru.share_apps.ShareAppsAndroidLibrary;
+
+public class ShareApps {
+    private static String startingKey;
+
+    public static void startWithKey(String key) {
+        startingKey = key;
+    }
+
+    public static void log(String msg) {
+        // TODO: log msg
+    }
+}
