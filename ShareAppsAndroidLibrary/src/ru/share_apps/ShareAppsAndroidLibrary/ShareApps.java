7fe1fa154a43f94ef06b8d775d388551500b1f61
svichkarev <tolik0393@mail.ru>
2014-10-25 17:42:40 +0700

    created simple catcher exception

--- ShareAppsAndroidLibrary/src/ru/share_apps/ShareAppsAndroidLibrary/ShareApps.java
+++ ShareAppsAndroidLibrary/src/ru/share_apps/ShareAppsAndroidLibrary/ShareApps.java
@@ -1,10 +1,24 @@
 package ru.share_apps.ShareAppsAndroidLibrary;
 
+import android.util.Log;
+
 public class ShareApps {
+    public static final String TAG = "ShareApps";
+
     private static String startingKey;
 
     public static void startWithKey(String key) {
         startingKey = key;
+
+        Thread.setDefaultUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler() {
+            @Override
+            public void uncaughtException(Thread paramThread, final Throwable e) {
+                final String CATCHING_HEADER = "Catching error: ";
+
+                Log.e(TAG, CATCHING_HEADER + e.toString());
+            }
+
+        });
     }
 
     public static void log(String msg) {
