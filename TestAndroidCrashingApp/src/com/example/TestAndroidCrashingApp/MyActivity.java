cf8f135734522b82e5ddbaeb39e889304d5482c5
svichkarev <tolik0393@mail.ru>
2014-10-25 17:57:49 +0700

    created Hello World test App for library

---
+++ TestAndroidCrashingApp/src/com/example/TestAndroidCrashingApp/MyActivity.java
@@ -0,0 +1,15 @@
+package com.example.TestAndroidCrashingApp;
+
+import android.app.Activity;
+import android.os.Bundle;
+import ru.share_apps.ShareAppsAndroidLibrary.ShareApps;
+
+public class MyActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        ShareApps.startWithKey("0011-0021-22001");
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
